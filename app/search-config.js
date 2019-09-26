708ddb24daeb3d6c768e4b01db1dd32e453cf395
Joe Lanman <joelanman@gmail.com>
2019-09-26 10:35:19 +0100

    WIP - adding minimatch

---
+++ app/search-config.js
@@ -0,0 +1,17 @@
+// Use this file to change search configuration.
+
+module.exports = {
+  // paths to include in search
+  // the search spider will follow links from these pages
+  include: [
+    '/'
+  ],
+
+  // paths to exclude in search, supports regular expressions
+  // for example:
+  // exclude : ['/admin*']
+  exclude: [
+    '*filter-suppliers'
+  ]
+
+}
